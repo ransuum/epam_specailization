215c57a4a03cb127e4dc7421269a84801532fba0
valerii_dmytrenko <valerii_dmytrenko@epam.com>
2025-03-19 22:43:51 +0200

    feat: appconfig and postproc update

--- src/main/java/org/epam/config/AppConfig.java
+++ src/main/java/org/epam/config/AppConfig.java
@@ -1,2 +1,46 @@
-package org.epam.config;public class AppConfig {
+package org.epam.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.persistence.EntityManager;
+import org.epam.models.SecurityContextHolder;
+import org.epam.models.entity.*;
+import org.epam.models.enums.UserType;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+
+@Configuration
+@ComponentScan(basePackages = {"org.epam", "org.epam.models.entity"})
+public class AppConfig {
+    @Bean
+    public ObjectMapper beanMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+
+    @Bean
+    public SecurityContextHolder securityContextHolder() {
+        return SecurityContextHolder.builder()
+                .userType(UserType.NOT_AUTHORIZE)
+                .build();
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setConfigLocation(new ClassPathResource("hibernate.cfg.xml"));
+        sessionFactory.setAnnotatedClasses(User.class, Trainee.class, Training.class, TrainingView.class, Trainer.class);
+        return sessionFactory;
+    }
+
+    @Bean
+    public EntityManager entityManager(SessionFactory sessionFactory) {
+        return sessionFactory.createEntityManager();
+    }
 }
--- src/main/java/org/epam/config/PostProcessing.java
+++ src/main/java/org/epam/config/PostProcessing.java
@@ -1,2 +1,31 @@
-package org.epam.config;public class PostProcessing {
+package org.epam.config;
+
+import jakarta.persistence.EntityManager;
+import lombok.NonNull;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:application.properties")
+public class PostProcessing implements BeanPostProcessor {
+    private static final Logger log = LogManager.getLogger(PostProcessing.class);
+
+    @Override
+    public Object postProcessBeforeInitialization(@NonNull Object bean,@NonNull String beanName) throws BeansException {
+        if (bean instanceof SessionFactory) log.info("Initializing SessionFactory: {}", beanName);
+        if (bean instanceof EntityManager) log.info("Initializing EntityManager: {}", beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
+        if (bean instanceof SessionFactory) log.info("SessionFactory initialized: {}", beanName);
+        if (bean instanceof EntityManager) log.info("EntityManager initialized: {}", beanName);
+        return bean;
+    }
 }
