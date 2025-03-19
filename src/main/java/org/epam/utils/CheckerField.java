ea03a39078a0a838be7d794080fe619beab88906
valerii_dmytrenko <valerii_dmytrenko@epam.com>
2025-03-19 22:43:19 +0200

    feat: update password and username generation

---
+++ src/main/java/org/epam/utils/CheckerField.java
@@ -0,0 +1,32 @@
+package org.epam.utils;
+
+
+import org.epam.models.enums.TrainingType;
+
+import java.time.LocalDate;
+
+public class CheckerField {
+    public static boolean check(String val) {
+        return val != null && !val.isEmpty() && !val.isBlank();
+    }
+
+    public static boolean check(Integer val) {
+        return val != null && val > 0;
+    }
+
+    public static boolean check(LocalDate val) {
+        return val != null;
+    }
+
+    public static boolean check(Boolean isActive) {
+        return isActive != null;
+    }
+
+    public static boolean check(TrainingType trainingType) {
+        return trainingType != null;
+    }
+
+    public static boolean check(Long duration) {
+        return duration != null;
+    }
+}
