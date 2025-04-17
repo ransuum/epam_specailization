691374f9867a6ccf50c447d8bcf41038c8dba784
valerii_dmytrenko <valerii_dmytrenko@epam.com>
2025-04-17 17:44:24 +0300

    fix: merge

--- src/main/java/org/epam/config/AppConfig.java
+++ src/main/java/org/epam/config/AppConfig.java
@@ -1,5 +1,7 @@
 package org.epam.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.epam.repository.UserRepository;
